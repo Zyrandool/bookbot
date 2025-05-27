d7f486b4feec4483854d3d96aedfeaaadec48cd8
Zyrandool <zyrafka22@gmail.com>
2025-05-27 23:46:26 +0200

    Raport and Filepath

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from stats import *
+from sys import argv, exit 
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
 def main():
-    filepath="books/frankenstein.txt"
+    if len(argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    filepath=argv[1]
+    if not filepath.endswith('.txt'):
+        print("Please provide a valid text file.")
+        sys.exit(1)
     text = get_book_text(filepath)
     num_words = get_num_words(text)
     char_count = get_char_number(text)
-    print(f"{num_words} words found in the document")
-    print(char_count)
-
+    print("============ BOOKBOT ============")
+    print(f"Reading book from {filepath}")
+    print("=================================")
+    display_stats(num_words, char_count)
 
 
 # Dodaj to wywołanie funkcji main()
--- stats.py
+++ stats.py
@@ -4,8 +4,22 @@ def get_num_words(text):
     return len(str.split(text))
 
 def get_char_number(text):
-    l = str.lower(text)
-    char_count = defaultdict(int)
-    for char in l:
-        char_count[l]+=1
+    lowertext = str.lower(text)
+    char_count = {}
+    for character in lowertext:
+        if(character in char_count):
+            char_count[character] = char_count[character] + 1
+        else:
+            char_count[character] = 1
     return char_count
+
+def display_stats(num_words, char_count):
+    sorted_chars = sorted(char_count.items(), key=lambda item: item[1], reverse=True)
+
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("-------- Character Count --------")
+    for char, count in sorted_chars:
+        if(char.isalpha()):
+            print(f"{char}: {count}")
+
