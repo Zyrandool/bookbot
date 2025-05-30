b06162c256d22825fcbf747b7187ca5e29e64a77
Zyrandool <zyrafka22@gmail.com>
2025-05-30 19:53:01 +0200

    Error Handling for invalid Filename

--- main.py
+++ main.py
@@ -2,17 +2,24 @@ from stats import *
 from sys import argv, exit 
 
 def get_book_text(filepath):
-    with open(filepath) as f:
-        return f.read()
+    try:
+        f = open(filepath, 'r', encoding='utf-8')
+    except FileNotFoundError:
+        print(f"Error: The file {filepath} does not exist.")
+        exit(1)
+    else:
+        with f:
+            # Read the entire file content
+            return f.read()
 
 def main():
     if len(argv) < 2:
         print("Usage: python3 main.py <path_to_book>")
-        sys.exit(1)
+        exit(1)
     filepath=argv[1]
     if not filepath.endswith('.txt'):
         print("Please provide a valid text file.")
-        sys.exit(1)
+        exit(1)
     text = get_book_text(filepath)
     num_words = get_num_words(text)
     char_count = get_char_number(text)
